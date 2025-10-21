03f8b25628d968453f1af9b416e3c4bd27dc4f4b
YourUsername <saraswathinandula@surnoi.com>
2025-10-21 16:44:59 +0530

    made changes

--- jenkins-master/main.tf
+++ jenkins-master/main.tf
@@ -7,11 +7,17 @@ data "aws_vpc" "default" {
   default = true
 }
 # Fetch all default subnets
-data "aws_subnet_ids" "default_vpc_subnets" {
-  vpc_id = data.aws_vpc.default.id
+# data "aws_subnet_ids" "subnet_ids" {
+#   vpc_id = data.aws_vpc.default.id
+# }
+data "aws_subnets" "default_vpc_subnets" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 locals {
-  default_subnet_id = data.aws_subnet_ids.default_vpc_subnets.ids[0]
+  subnet_ids = data.aws_subnets.default_vpc_subnets.ids
 }
  
 # Fetch latest Amazon Linux 2 AMI
@@ -41,7 +47,7 @@ module "ec2" {
   source            = "../modules/ec2"
   ami_id            = data.aws_ami.amazon_linux.id
   key_name          = "devops"  # Replace with your key pair
-  subnet_id         = local.default_subnet_id
+  subnet_id         = local.subnet_ids
   vpc_id            = data.aws_vpc.default.id
   jenkins_master_sg = module.sg.jenkins_master_sg_id
   backend_sg        = module.sg.backend_sg_id
