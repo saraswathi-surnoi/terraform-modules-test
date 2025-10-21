44d06bf30ef7c54eb8cae7dec82602b2dc1f5a8d
YourUsername <saraswathinandula@surnoi.com>
2025-10-21 15:54:12 +0530

    made changes

--- modules/ec2/variables.tf
+++ modules/ec2/variables.tf
@@ -1,5 +1,18 @@
 variable "ami_id" {}
-variable "key_name" {}
+variable "key_name" {
+  description = "Existing AWS key pair name to use for EC2 instances"
+  type        = string
+  default     = "devops"
+}
+data "aws_key_pair" "selected" {
+  key_name = var.key_name
+}
+locals {
+  key_pair_exists = length(data.aws_key_pair.selected.id) > 0
+}
+output "key_pair_name" {
+  value = data.aws_key_pair.selected.key_name
+}
 variable "subnet_id" {}
 variable "vpc_id" {}
 variable "jenkins_master_sg" {}
